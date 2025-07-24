a13fbe478cd3b36d8df4ba7660c32e00ba785aff
chris <bbhriss@gmail.com>
2025-07-23 21:29:16 -0700

    Updated Changes

---
+++ CamTest.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("best.pt")
+
+# start webcam at 0 (default camera)
+cap = cv2.VideoCapture(1)
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # make detections
+    results = model(frame, conf=0.25)[0]
+    annotated_frame = results.plot()
+
+    # display the frame with detections
+    cv2.imshow("Webcam Feed", annotated_frame)
+
+    # exit on 'q' key press
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
